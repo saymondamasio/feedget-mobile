9f0ea7fc3a84d5a49689088fd225c80c41e0cca3
Saymon Damásio <saymon.damasio95@gmail.com>
2022-05-06 18:18:41 -0300

    feat: add copyright component

---
+++ src/components/Copyright/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+import { Container } from './styles'
+
+export function Copyright() {
+  return <Container>Feito com ♥ pela Rocketseat</Container>
+}
---
+++ src/components/Copyright/styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components/native'
+import { theme } from '../../theme'
+
+export const Container = styled.Text`
+  font-size: 12px;
+  color: ${theme.colors.text_secondary};
+  font-family: ${theme.fonts.medium};
+`
