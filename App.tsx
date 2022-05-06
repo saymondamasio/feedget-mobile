81d7b5cd3375f2844a90e3dad31268bcd831d698
Saymon Damásio <saymon.damasio95@gmail.com>
2022-05-06 18:17:25 -0300

    feat: init

--- App.tsx
+++ App.tsx
@@ -1,20 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native'
+import { Widget } from './src/components/Widget'
+
+import * as Font from 'expo-font'
+
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_500Medium,
+} from '@expo-google-fonts/inter'
+import * as SplashScreen from 'expo-splash-screen'
+import { useCallback, useEffect, useState } from 'react'
+import { StatusBar } from 'expo-status-bar'
+import { theme } from './src/theme'
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [appIsReady, setAppIsReady] = useState(false)
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await SplashScreen.preventAutoHideAsync()
+
+        await Font.loadAsync({
+          Inter_400Regular,
+          Inter_500Medium,
+        })
+      } catch (e) {
+        console.warn(e)
+      } finally {
+        setAppIsReady(true)
+      }
+    }
+
+    prepare()
+  }, [])
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync()
+    }
+  }, [appIsReady])
+
+  if (!appIsReady) {
+    return null
+  }
+
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <View
+        style={{ flex: 1, backgroundColor: theme.colors.background }}
+        onLayout={onLayoutRootView}
+      >
+        <Widget />
+
+        <StatusBar style="light" backgroundColor="transparent" translucent />
+      </View>
+    </GestureHandlerRootView>
+  )
+}
